46ff29aa36f879ad1ec202ff7cd29afcc17f96e8
Joshua M. Boniface <joshua@boniface.me>
2023-03-15 02:13:11 -0400

    Add LICENSE and header text

--- hwatest/hwatest.py
+++ hwatest/hwatest.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+# hwatest.py
+# A CPU and Hardware Acceleration (GPU) tester for Jellyfin
+#
+#    Copyright (C) 2023 Joshua M. Boniface <joshua@boniface.me>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 import click
 import os
 import urllib.request
